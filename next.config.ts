601d31bd0565d1c4205dc7eb118c7309705c089b
khuni <127084199+khuni1@users.noreply.github.com>
2025-05-22 12:32:55 +0900

    Update next.config.ts
    로컬 테스트용과 쿠버네티스 배포용 중 원하는 설정의 주석 처리를 해제하면됩니다.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   env: {
+      /*로컬 테스트용 */
+      //NEXT_PUBLIC_API_URL:"http://localhost:8081"
+
+      /*쿠버네티스 배포용*/
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
